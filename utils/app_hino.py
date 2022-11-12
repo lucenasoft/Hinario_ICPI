46c6b270cd3beaf747c05f9e68bdf89f20182b2a
lucenasoft <getugbr@gmail.com>
2022-11-12 17:54:57 -0300

    hcp organizado

--- utils/app_hino.py
+++ utils/app_hino.py
@@ -1,30 +1,28 @@
 from time import sleep
 
-import pdfplumber as pdf
-
+louvores = list()
 louvor = list()
 
+with open('utils/hcp.txt',encoding="utf8") as f:
+    content = f.readlines()
+
+for x in content:
+    louvor.append(x.rstrip('\n'))
+print(louvor[0:10])
+
+
+'''
 cont_pdf = 1
-cont_louvor = 2
-cont_pag = 3
+l_l = 2
 
-pdf_open = pdf.open('utils/hcp.pdf')
 
-while cont_pag < 401:
-    page = pdf_open.pages[cont_pag]
-    text = page.extract_text()
-    pagina = text.split('\n')
+for l in louvores:
+    if f'{l_l}.' not in l:
+        louvor.append(l) 
 
-    for l in pagina:
-
-        while f'{cont_louvor}.' not in l:
-            louvor.append(l)
-            break   
-
-        if f'{cont_louvor}.' in l:
-            with open(f'utils/hinos/{cont_pdf}.txt', 'w') as arquivo:
-                arquivo.write("\n".join(louvor))
-                cont_pdf += 1
-                louvor.clear()
-            cont_louvor += 1
-            cont_pag += 1
+    if f'{l_l}.' in l:
+        with open(f'utils/hinos/{cont_pdf}.txt', 'w') as arquivo:
+            arquivo.write("\n".join(louvor))
+            cont_pdf += 1
+            louvor.clear()
+        l_l += 1'''
