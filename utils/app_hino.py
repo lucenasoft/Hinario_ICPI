be2d5a0b0a4bbbfce585f2a8d4e9f6574e432962
lucenasoft <getugbr@gmail.com>
2022-11-12 16:02:36 -0300

    lucas_cabeça_de_coco

--- utils/app_hino.py
+++ utils/app_hino.py
@@ -4,18 +4,27 @@ import pdfplumber as pdf
 
 louvor = list()
 
+cont_pdf = 1
+cont_louvor = 2
+cont_pag = 3
+
 pdf_open = pdf.open('utils/hcp.pdf')
 
-page = pdf_open.pages[329]
-text = page.extract_text()
-pagina = text.split('\n')
+while cont_pag < 401:
+    page = pdf_open.pages[cont_pag]
+    text = page.extract_text()
+    pagina = text.split('\n')
 
-for l in pagina:
-    if '2. ' not in l:
-        louvor.append(l)
-    else:
-        break
-        
+    for l in pagina:
 
-with open('Louvores.txt', 'w') as arquivo:
-    arquivo.write("\n".join(louvor))
+        while f'{cont_louvor}.' not in l:
+            louvor.append(l)
+            break   
+
+        if f'{cont_louvor}.' in l:
+            with open(f'utils/hinos/{cont_pdf}.txt', 'w') as arquivo:
+                arquivo.write("\n".join(louvor))
+                cont_pdf += 1
+                louvor.clear()
+            cont_louvor += 1
+            cont_pag += 1
