8006ceee61572c2cc9e8dc2d8e854964a588ebae
lucenasoft <getugbr@gmail.com>
2022-11-11 22:43:45 -0300

    todos os titulos separados

--- utils/app.py
+++ utils/app.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pdfplumber as pdf
 
 titulo = list()
@@ -6,29 +8,27 @@ pdf_open = pdf.open('utils/hcp.pdf')
 
 
 v_t = []
-cont = 0
-
-page = pdf_open.pages[383]
-text = page.extract_text()
-pagina = text.split('\n')
-
-while cont < 400:
-    cont += 1
+cont = 291
 
 def v_titulo():
-    for l in pagina:
-        if l[0].isdigit() == True:
-            v_t.append(f'{l[0]}. ')
-
-        if l[0:2].isdigit() == True:
-            v_t.append(f'{l[0:2]}. ')
-
-        if l[0:3].isdigit() == True:
-            v_t.append(f'{l[0:3]}. ')
-
-    for i in v_t:
+    global cont
+    global pagina
+    global titulo
+    while cont < 399:
+        page = pdf_open.pages[cont]
+        text = page.extract_text()
+        pagina = text.split('\n')
         for l in pagina:
-            if i in l:
-                print(l)
+            if l[0:3].isdigit() == True:
+                v_t.append(f'{l[0:3]}. ')
+        
+        for i in sorted(set(v_t)):
+            for l in pagina:
+                if i in l:
+                    titulo.append(f'{l}')
+        cont += 1
 
 v_titulo()
+
+with open('1-9.txt', 'w') as arquivo:
+    arquivo.write("\n".join(titulo))
--- utils/teste.py
+++
@@ -1,5 +0,0 @@
-import aspose.words as aw
-
-doc = aw.Document("utils/hcp.pdf")
-
-doc.save("teste.html")
