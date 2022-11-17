8871ceea29376af6d9e564a13bc4931fc64f0bcc
lucenasoft <getugbr@gmail.com>
2022-11-17 13:03:18 -0300

    feat: base_static creation and configuration

--- icpi/settings.py
+++ icpi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mddjy*$3g#jphx)ikyxf&(8zf6fbkbs&f=@ondof5-s9et*8od
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.56.1','hrrbc023']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'hinario/static/'
+STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "base_static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
