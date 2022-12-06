66a094aad58c014da99509bd7c9a7a65d3718ca4
lucenasoft <getugbr@gmail.com>
2022-12-06 18:21:41 -0300

    feat: function search button is active

--- hinario/views.py
+++ hinario/views.py
@@ -5,38 +5,13 @@ from hinario.models import Hino
 # Create your views here.
 
 def hinos01(request):
-    hinos01 = Hino.objects.all()[:100]
+    hinos01 = Hino.objects.all()
+    search = request.GET.get('search')
+    if search:
+        hinos01 = hinos01.filter(titulo__icontains=search)
     return render(request,'hinos01.html',context={
         'hinos01':hinos01,
         'select_page': 1,
-    })
-
-def hinos02(request):
-    hinos02 = Hino.objects.all()[100:200]
-    return render(request,'hinos02.html',context={
-        'hinos02':hinos02,
-        'select_page': 2,
-    })
-
-def hinos03(request):
-    hinos03 = Hino.objects.all()[200:300]
-    return render(request,'hinos03.html',context={
-        'hinos03':hinos03,
-        'select_page': 3,
-    })
-
-def hinos04(request):
-    hinos04 = Hino.objects.all()[300:400]
-    return render(request,'hinos04.html',context={
-        'hinos04':hinos04,
-        'select_page': 4,
-    })
-
-def hinos05(request):
-    hinos05 = Hino.objects.all()[400:500]
-    return render(request,'hinos05.html',context={
-        'hinos05':hinos05,
-        'select_page': 5,
     })
 
 def hino(request,id):
